bdf3b835c2b22f69f5f5bf3b7b4b26469305de8d
salamelek <samuel.sluga@gmail.com>
2023-07-26 19:20:45 +0200

    sub code this time for real i guess

---
+++ arduino_code/submarine_code/submarine_code.ino
@@ -0,0 +1,53 @@
+// submarine code by salamelek
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// how much time can the sub go without signal (in seconds)
+int emergencyTimeOut = 3;
+
+RF24 radio(9,10); // CE, CSN
+const byte address[6] = "00001";
+char data[32];
+
+void setup() {
+  Serial.begin(230400);
+
+  radio.begin();
+  radio.setPayloadSize(9);
+  radio.openReadingPipe(1,address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening();
+
+  Serial.println("Receiver running...");
+}
+
+void printArray(unsigned char *arr, int size) {
+  Serial.print("[");
+  for (int i = 0; i < size; i++) {
+    Serial.print(arr[i]);
+    if (i < size - 1) {
+      Serial.print(", ");
+    }
+  }
+  Serial.println("]");
+}
+
+void loop() {
+  unsigned long previousTime = 0;
+  unsigned long currentTime = millis();
+
+  if(radio.available()) {
+    unsigned char receivedData[9];                    // Create a buffer to store the received data
+    radio.read(&receivedData, sizeof(receivedData));  // Read the received data into the buffer
+
+    // Print the received array
+    Serial.print("Received data: ");
+    printArray(receivedData, sizeof(receivedData));
+  }
+
+  if (currentTime - previousTime > emergencyTimeOut * 1000) {
+    
+  }
+}
